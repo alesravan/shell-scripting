38920fd99b32086e2e3341940a21af4fc734e152
sravan <alesravan@gmail.com>
2023-06-07 23:15:22 -0400

    Training

--- 13-if-else-condition.sh
+++ 13-if-else-condition.sh
@@ -3,13 +3,13 @@ input =$1
 text =$2
 
 # Simple If
-if [$input/2=0];then
+if [$((input))/2=0];then
     echo "Even number"
 fi
 
 # if else
 
-if [$input/2=0];then
+if [$((input))/2=0];then
     echo "Even number"
 else
     echo "Odd Number"
@@ -17,9 +17,9 @@ fi
 
 #else if
 
-if [$text=="start" ];then
+if ["$text"=="start" ];then
     echo "chose start"
-elif [$text=="stop" ];then
+elif ["$text"=="stop" ];then
     echo "chose stop"
 else
     echo "Chosen option not available"
