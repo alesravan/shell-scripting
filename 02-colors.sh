67e8287e1b86bc6e1c4ea9e8cb388e97980a9f7d
sravan <alesravan@gmail.com>
2023-06-07 22:36:52 -0400

    Training

---
+++ 02-colors.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+
+echo -e "\e[31m printing green color \e[0m"
+echo -e "\e[32m printing green color \e[0m"
+echo -e "\e[33m printing Yellow color \e[0m"
+echo -e "\e[34m printing Blue color \e[0m"
+echo -e "\e[35m printing Meganta color \e[0m"
+echo -e "\e[36m printing Cyan color \e[0m"
