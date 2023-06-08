55fff83829f8cd43e4a94a9dc3c0fc08a67ee2d9
sravan <alesravan@gmail.com>
2023-06-07 23:11:09 -0400

    Training

---
+++ 13-if-else-condition.sh
@@ -0,0 +1,29 @@
+#!bin/bash
+input =$1
+text =$2
+
+# Simple If
+if [$input/2=0];then
+    echo "Even number"
+fi
+
+# if else
+
+if [$input/2=0];then
+    echo "Even number"
+else
+    echo "Odd Number"
+fi
+
+#else if
+
+if [$text=="start" ];then
+    echo "chose start"
+elif [$text=="stop" ];then
+    echo "chose stop"
+else
+    echo "Chosen option not available"
+fi
+
+
+
